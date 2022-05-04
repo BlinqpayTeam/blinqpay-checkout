4ec890aeed0e630ed6fb579f0504608f7e35e346
Ayodele <daylay92@yahoo.com>
2022-05-04 14:41:25 +0100

    fix(build): fix background

--- src/ui/components/Layout/style.ts
+++ src/ui/components/Layout/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container: any = styled.div`
-  background: #e5e5e5;
+  background: #00000026;
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
